a405869415f532bd7c9e5c4c9102baa55abac0ad
marusa <redblack23422@gmail.com>
2026-04-24 17:39:36 +0300

    edit palataScreens

--- app/src/main/java/com/example/ppmob/presentation/screen/Step2Screen.kt
+++ app/src/main/java/com/example/ppmob/presentation/screen/Step2Screen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -57,11 +58,20 @@ fun Step2Screen(navController: NavHostController) {
 
             Box(
                 modifier = Modifier
-                    .padding(start = 190.dp, top = 65.dp)
+                    .align(Alignment.TopStart)
+                    .layout { measurable, constraints ->
+                        val placeable = measurable.measure(constraints)
+                        val parentWidth = constraints.maxWidth
+                        val parentHeight = constraints.maxHeight
+                        val x = (parentWidth * 0.44f).toInt()
+                        val y = (parentHeight * 0.10f).toInt()
+                        layout(placeable.width, placeable.height) {
+                            placeable.place(x, y)
+                        }
+                    }
                     .size(50.dp)
                     .clip(CircleShape)
                     .background(Color.White)
-                    .align(Alignment.TopStart)
                     .clickable { navController.navigate(NavRoutes.step21) },
                 contentAlignment = Alignment.Center
             ) {
