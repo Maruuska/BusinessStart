e84ea71cb418ad4963a1493fed180c0d4e33cd8d
marusa <redblack23422@gmail.com>
2026-06-10 20:37:08 +0300

    add readme

--- app/src/main/java/com/example/ppmob/domain/state/SignInState.kt
+++ app/src/main/java/com/example/ppmob/domain/state/SignInState.kt
@@ -1,8 +1,8 @@
 package com.example.ppmob.domain.state
 
 data class SignInState(
-    val email: String = "test@test.com",
-    val password: String = "12345678",
+    val email: String = "",
+    val password: String = "",
     var errorEmail:Boolean = false,
     var errorPassword:Boolean = false,
     val passwordVisible:Boolean = false
